f04cdcf46666f836107f923ae9d75ea5f38154b3
shago420 <shago123-@hotmail.com>
2024-03-18 08:05:55 -0500

    from socketio

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-si
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketIO = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketIO.on('message')
+def handle_message(msg):
+    print(msg)
+    socketIO.emit('message', msg)
+
+if __name__ == "_main_":
+    socketIO.run( app,debug=True)
+    
